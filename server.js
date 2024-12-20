0dfc9ed7e4fe587c33594772b53d660223f5e7c7
MohsinSheeraz <syedmohsinsheeraz123@gmail.com>
2024-12-20 05:51:10 +0500

    fix: "Changes"

--- server.js
+++ server.js
@@ -191,7 +191,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
